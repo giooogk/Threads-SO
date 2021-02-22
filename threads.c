14f32aee58c7004293df9e674f82065ef183e304
Giovanna Karla <47827014+giooogk@users.noreply.github.com>
2021-02-22 19:34:12 -0300

    controle de threads criadas e trapezios

--- threads.c
+++ threads.c
@@ -9,25 +9,49 @@ void *hello_Word(void *tid){
     pthread_exit(NULL);
 }
 
+void *regra_Trapezio(void *tid){
+    
+    h = (b-a)/n;
+    area_total = (f(a) + f(b)) / 2;
+    
+    for(int i = 1; i < n; i ++){
+        x_i = a + i * h;
+        area_total += f(x_i);
+    }
+    
+    area_total += h * area_total;
+}
+
 int main(int argc, char *argv[]){
     
-    pthread_t thread;
-    int status;
+    int status,numthreads, numtrapezios, i;
     void *thread_return;
 
-    printf("Processo principal criando thread\n");
-    status = pthread_create(&thread, NULL, hello_Word, 0);
+    printf("Digite o numero de threads: ");
+    scanf("%d", &numthreads);
+
+    printf("Digite o numero de Trapezios: ");
+    scanf("%d", &numtrapezios);
     
-    if(status!=0){
-        printf("Erro na criação da thread.  Codigo de Erro%d\n", status);
-        return 1;
+    pthread_t threads[numthreads];
+    
+    for(i=0; i<numthreads; i++){
+        printf("Processo principal criando thread#%d\n", i);
+        status = pthread_create(&threads[i], NULL, hello_Word, (void*)(size_t) i);
+
+        if(status != 0){
+            printf("Erro na criaçao da thread. Codigo de Erro:%d\n", status);
+            return 1;
+        }
+
     }
 
-    printf("Esperando Thread finalizar....\n");
-    pthread_join(thread, &thread_return);
-    printf("Thread finalizada\n");
+    for(i=0; i<numthreads; i++){
+        printf("Esperando Thread %d finalizar....\n", i);
+        pthread_join(threads[i], &thread_return);
+        printf("Thread %d finalizada\n", i);
+    }
 
-    printf("processo pai vai finalizar\n");
+    printf("Processo vai finalizar\n");
 
-    return 0;
 }
